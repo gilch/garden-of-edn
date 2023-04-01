f81110a5c38699bd648832dd738690488821f923
gilch <gilch@users.noreply.github.com>
2023-03-31 21:02:35 -0600

    Add MPL-2.0 notice

--- garden_of_edn.py
+++ garden_of_edn.py
@@ -1,4 +1,8 @@
 # Copyright 2022, 2023 Matthew Egan Odendahl
+# SPDX-License-Identifier: MPL-2.0
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """A Garden of EDN parsers. Bringing the Python to EDN.
 
 These parsers are not validators. Behavior when given invalid EDN is
